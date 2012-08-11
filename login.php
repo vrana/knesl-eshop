beafd013e79818377413535040379fc5a227ea9f
Jakub Vrana <jakub@vrana.cz>
2012-08-11 13:21:15 -0700

    Session fixation protection

--- login.php
+++ login.php
@@ -1,6 +1,7 @@
 <?php
 $row = query("SELECT COUNT(*) FROM users WHERE username = %s AND password = SHA1(CONCAT(%s, salt))", $_POST["username"], $_POST["password"])->fetchColumn();
 if ($row) {
+	session_regenerate_id();
 	$_SESSION["username"] = $_POST["username"];
 	redirect('');
 }
