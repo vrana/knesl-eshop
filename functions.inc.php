f59835a1d4baa6c36d05fc066028792642c64b46
Jakub Vrana <jakub@vrana.cz>
2012-08-11 11:52:29 -0700

    Separate HTML

--- functions.inc.php
+++ functions.inc.php
@@ -55,39 +55,9 @@ function rsrc($file) {
 	return href("static/$file");
 }
 
-function htmlHead($title) {
-	?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN">
-<html lang="en">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title><?=h($title)?></title>
-<link rel="stylesheet" type="text/css" href="<?=h(rsrc('default.css'))?>">
-<script type="text/javascript" src="<?=h(rsrc('functions.js'))?>"></script>
-<body>
-
-<? if (!isset($_SESSION["username"])) { ?>
-	<form action="<?=href('login/')?>" method="post">
-	<p>
-	<input name="username" value="<?=h($_POST["username"])?>">
-	<input type="password" name="password">
-	<input type="submit" value="Login">
-	</p>
-	</form>
-<? } else { ?>
-	<form action="<?=href('logout/')?>" method="post">
-	<p>
-	Username: <b><?=h($_SESSION["username"])?></b>
-	<input type="submit" value="Logout">
-	</p>
-	</form>
-<? } ?>
-
-<h1><?=h($title)?></h1>
-
-<?php
-}
-
 /** htmlspecialchars($s, ENT_QUOTES) */
 function h($s) {
 	return htmlspecialchars($s, ENT_QUOTES);
 }
+
+include "./html.inc.php";
