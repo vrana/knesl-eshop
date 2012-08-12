735c6359c738435059d24998ea5f4c795bff7377
Jakub Vrana <jakub@vrana.cz>
2012-08-11 19:31:31 -0700

    XML export

--- admin/index.php
+++ admin/index.php
@@ -2,7 +2,8 @@
 function adminer_object() {
 	
 	class AdminerEshop extends Adminer {
-		var $product_ids = array();
+		private $product_ids = array();
+		private $database = false;
 		
 		function name() {
 			return '<a href=".." id="h1">eShop</a>';
@@ -89,6 +90,72 @@ setInterval(function () {
 			parent::navigation($missing);
 		}
 		
+		function dumpFormat() {
+			return array('xml' => 'XML') + parent::dumpFormat();
+		}
+
+		function dumpData($table, $style, $query) {
+			if ($_POST["format"] != "xml") {
+				return parent::dumpData($table, $style, $query);
+			}
+			if (!$this->database) {
+				$this->database = true;
+				echo "<database name='" . h(DB) . "'>\n";
+				register_shutdown_function(array($this, '_database'));
+			}
+			$rows = get_rows($query);
+			$order_items = array();
+			
+			if ($table == "orders") {
+				$orders_ids = array();
+				foreach ($rows as $row) {
+					$orders_ids[] = $row["id"];
+				}
+				if ($orders_ids) {
+					$connection = connection();
+					$result = $connection->query("
+						SELECT order_items.orders_id, products.id, products.name, order_items.amount, order_items.price
+						FROM order_items
+						INNER JOIN products ON order_items.products_id = products.id
+						WHERE order_items.orders_id IN (" . implode(", ", $orders_ids) . ")
+					");
+					while ($row = $result->fetch_assoc()) {
+						$orders_id = $row["orders_id"];
+						unset($row["orders_id"]);
+						$order_items[$orders_id][] = $row;
+					}
+				}
+			}
+			
+			foreach ($rows as $row) {
+				echo "\t<table name='" . h($table) . "'>\n";
+				foreach ($row as $key => $val) {
+					echo "\t\t<column name='" . h($key) . "'>" . h($val) . "</column>\n";
+				}
+				if (array_key_exists($row["id"], $order_items)) {
+					foreach ($order_items[$row["id"]] as $item) {
+						echo "\t\t<table name='order_items'>\n";
+						foreach ($item as $key => $val) {
+							echo "\t\t\t<column name='" . h($key) . "'>" . h($val) . "</column>\n";
+						}
+						echo "\t\t</table>\n";
+					}
+				}
+				echo "\t</table>\n";
+			}
+		}
+
+		function _database() {
+			echo "</database>\n";
+		}
+		
+		function dumpHeaders($identifier, $multi_table = false) {
+			if ($_POST["format"] != "xml") {
+				return parent::dumpHeaders($identifier, $multi_table);
+			}
+			header("Content-Type: text/xml; charset=utf-8");
+			return "xml";
+		}
 	}
 	
 	return new AdminerEshop;
